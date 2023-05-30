7348b51a789aab72d56e5a8535caf6a121bb22e7
ismailelmasry <i.elmasry@student.tue.nl>
2023-05-30 17:35:39 +0200

    Merge branch 'main' of https://github.com/Sarthakushirke/Ball_and_plate

--- General/Project/main.m
+++ General/Project/main.m
@@ -1,53 +1,86 @@
-clear all, close all, clc
+clc; clear; close all;
 
-lx = 39/100; 
-ly = 39/100;
-m_slider = 0.135; % kg
-depth = 34.8/1000; 
-thickness = 4.8/1000; 
-V_outer = lx*ly*depth;
-V_inner = (lx-2*thickness)*(ly-2*thickness)*(depth-thickness);
-rho = 1180; %kg/m^3
-V = (V_outer-V_inner);
-m_plate = V*rho;
+% Calculation of the mass
+rho = 1180;                 % [kg/m^3]
+plate_length = 0.39;        % [m]
+side_plate_height = 0.035;  % [m]
+thickness = 0.0048 ;        % [m]
 
-% Model
-s = tf('s');
-kt = 11; % N/A motor constant
-m_tot = m_slider+m_plate/3; 
-d = 0; % Motor friction
-sys = kt/(m_tot*s^2+d*s);
+side_plate_m3 = plate_length * side_plate_height * thickness;   % [m^3]
+foundation_m4 = plate_length * plate_length * thickness;        % [m^3]
+total_m3 = 4 * side_plate_m3 + foundation_m4;                   % [m^3]
+
+m_plate = total_m3*rho; % [kg]
+
+% Paramters of the motor model
+Kt = 11;                % [N/A] - motor constant
+m = 0.135 + m_plate/3;  % [kg] - mass to be moved by the motor (approximate)
+D = 0;                  % [Ns/m] % Vicious friction - Didn't know, so for now we assume no friction (we could see that this was not the actual
+
+num = [Kt];
+den = [m D 0];
+
+% Motor model
+sys = tf(num,den);
 bode(sys)
-num_p = cell2mat(sys.num);
-den_p = cell2mat(sys.den);
 
 %% Load controller
 load('controller_lowpass.mat') % No integrator  
 % C = shapeit_data.C_tf_z;
 C = shapeit_data.C_tf;
-C = tf(C);
+C_shapeit = tf(C);
 num_c = cell2mat(C.num);
 den_c = cell2mat(C.den);
+s = tf('s');
 
+lead_zero = 3.33;
+lead_pole = 30;
+lpf_pole = 100;
+damping = 0.6;
+gain = 100;
 
-%% Nyquist robustness
-m_max = 0.150*100000;
-delta = 0:m_max/10:m_max;
-figure
-for i = 1:length(delta)
-sys = kt/((m_tot+delta(i))*s^2+d*s);
-L = C*sys;
-nyquist(1+L)
+C_gain = gain;
+C_lead = ((1/(2*pi*lead_zero))*s+1)/((1/(2*pi*lead_pole))*s+1);
+C_lpf = 1/((1/(2*pi*lpf_pole)^2)*s^2 + ((2*damping)/(2*pi*lpf_pole))*s+1);
+C = C_gain*C_lead*C_lpf;
+bode(C_shapeit)
 hold on
-end
+bode(C)
 
-figure
-for i = 1:length(delta)
-sys = kt/((m_tot+delta(i))*s^2+d*s);
-trans = loopsens(C,sys);
-pzmap(trans.Ti)
-hold on
-end
+% Control parameters for Simulink model
+numc_mi = C.Numerator{:};
+denc_mi = C.Denominator{:};
 
-% m_tot_aux = m_tot + delta 
-
+%%
+% %% Nyquist robustness
+% m_max = 0.150*100000;
+% delta = 0:m_max/10:m_max;
+% figure
+% for i = 1:length(delta)
+% sys = kt/((m_tot+delta(i))*s^2+d*s);
+% L = C*sys;
+% nyquist(1+L)
+% hold on
+% end
+% 
+% figure
+% for i = 1:length(delta)
+% sys = kt/((m_tot+delta(i))*s^2+d*s);
+% trans = loopsens(C,sys);
+% pzmap(trans.Ti)
+% hold on
+% end
+% 
+% % m_tot_aux = m_tot + delta 
+% %% Load controller
+% 
+% load('weak_PD.mat')
+% C = shapeit_data.C_tf;
+% C = tf(C);
+% num_c = cell2mat(C.num);
+% den_c = cell2mat(C.den);
+% 
+% C = shapeit_data.C_tf_z;
+% C = tf(C);
+% num_d = cell2mat(C.num);
+% den_d = cell2mat(C.den);
